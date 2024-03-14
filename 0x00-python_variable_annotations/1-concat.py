9f11a22114299dbcb03f4861b0315c94b15e4ee0
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-03-14 21:16:49 +0100

    add concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""1-concat module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates 2 strings
+
+    Args:
+        str1 (str): _description_
+        str2 (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    return str1 + str2
