cbb9708a44472fc2235ba4692a1cbdf7a927173e
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-03-14 21:22:13 +0100

    add floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""2-floor.py module"""
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor of a float
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        int: _description_
+    """
+
+    return math.floor(n)
