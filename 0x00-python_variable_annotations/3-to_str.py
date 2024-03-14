bbb0b635ed0ca5c16c6ad370701cc04708e5f75b
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-03-14 21:28:29 +0100

    add to_str function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""3-to_str module"""
+
+
+def to_str(n: float) -> str:
+    """_summary_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        str: _description_
+    """
+    return str(n)
