c3269be88930cbb36366f1f4bc4a44a507a4cb7d
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-03-14 21:12:06 +0100

    Add initial implementation of add function with variable annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""0-add.py module"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two floats
+
+    Args:
+        a (float):
+        b (float):
+
+    Returns:
+        float: sum
+    """
+    return a + b
