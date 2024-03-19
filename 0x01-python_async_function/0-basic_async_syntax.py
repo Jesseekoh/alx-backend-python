7e1a1a4a98dfacb14d00d0eee5790190b9778867
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-03-19 01:41:46 +0100

    Add basic async syntax implementation

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""0-basic_async_syntax module"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Waits a random number of seconds"""
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
