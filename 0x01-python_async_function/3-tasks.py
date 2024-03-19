5895a7a97ca316ab4c0004cc624058cdb926ef2d
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-03-19 02:17:36 +0100

    Refactor task_wait_random function

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,6 +6,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_random(max_delay: int) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """creates an async task"""
     return asyncio.create_task(wait_random(max_delay))
